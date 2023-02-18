2a7eb24f2c975d491786506a1a831429d92a8c08
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-02-18 17:18:41 +0600

    Mouse hover random color added

--- js/style.js
+++ js/style.js
@@ -1,40 +1,44 @@
 // function
-document.getElementById('triangle-calc').addEventListener('click',function(){
+document.getElementById('triangle-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('triangle-b');
     const getValueTwo = getInputFieldValueById('triangle-h');
+    if(getValueOne,getValueTwo < 0){
+        alert("sorry");
+        return;
+    }
     const areaOfTriangle = 0.5 * getValueOne * getValueTwo;
     const areaTriangle = areaOfTriangle.toFixed(2);
     setAreaInAreaField('area-output1', areaTriangle);
 });
-document.getElementById('rectangle-calc').addEventListener('click',function(){
+document.getElementById('rectangle-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('rectangle-w');
     const getValueTwo = getInputFieldValueById('rectangle-l');
     const areaOfRectangle = getValueOne * getValueTwo;
     const areaRectangle = areaOfRectangle.toFixed(2);
     setAreaInAreaField('area-output2', areaRectangle);
 });
-document.getElementById('parallelogram-calc').addEventListener('click',function(){
+document.getElementById('parallelogram-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('parallelogram-b');
     const getValueTwo = getInputFieldValueById('parallelogram-h');
     const areaOfParallelogram = getValueOne * getValueTwo;
     const areaParallelogram = areaOfParallelogram.toFixed(2);
-    setAreaInAreaField('area-output3',areaParallelogram);
+    setAreaInAreaField('area-output3', areaParallelogram);
 });
-document.getElementById('rhombus-calc').addEventListener('click',function(){
+document.getElementById('rhombus-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('rhombus-d1');
     const getValueTwo = getInputFieldValueById('rhombus-d2');
     const areaOfRhombus = 0.5 * getValueOne * getValueTwo;
     const areaRhombus = areaOfRhombus.toFixed(2);
     setAreaInAreaField('area-output4', areaRhombus);
 });
-document.getElementById('pentagon-calc').addEventListener('click',function(){
+document.getElementById('pentagon-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('pentagon-p');
     const getValueTwo = getInputFieldValueById('pentagon-b');
     const areaOfPentagon = 0.5 * getValueOne * getValueTwo;
     const areaPentagon = areaOfPentagon.toFixed(2);
     setAreaInAreaField('area-output5', areaPentagon);
 });
-document.getElementById('ellipse-calc').addEventListener('click',function(){
+document.getElementById('ellipse-calc').addEventListener('click', function () {
     const getValueOne = getInputFieldValueById('ellipse-b');
     const getValueTwo = getInputFieldValueById('ellipse-h');
     const areaOfEllipse = 3.1416 * getValueOne * getValueTwo;
@@ -42,4 +46,40 @@ document.getElementById('ellipse-calc').addEventListener('click',function(){
     setAreaInAreaField('area-output6', areaEllipse);
 });
 
- 
+document.getElementById('card1').addEventListener("mouseover", function(){
+    document.getElementById('card1').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card1').addEventListener('mouseleave',function(){
+    document.getElementById('card1').style.backgroundColor = "white";
+  })
+  document.getElementById('card2').addEventListener("mouseover", function(){
+    document.getElementById('card2').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card2').addEventListener('mouseleave',function(){
+    document.getElementById('card2').style.backgroundColor = "white";
+  })
+  document.getElementById('card3').addEventListener("mouseover", function(){
+    document.getElementById('card3').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card3').addEventListener('mouseleave',function(){
+    document.getElementById('card3').style.backgroundColor = "white";
+  })
+  document.getElementById('card4').addEventListener("mouseover", function(){
+    document.getElementById('card4').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card4').addEventListener('mouseleave',function(){
+    document.getElementById('card4').style.backgroundColor = "white";
+  })
+  document.getElementById('card5').addEventListener("mouseover", function(){
+    document.getElementById('card5').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card5').addEventListener('mouseleave',function(){
+    document.getElementById('card5').style.backgroundColor = "white";
+  })
+  document.getElementById('card6').addEventListener("mouseover", function(){
+    document.getElementById('card6').style.backgroundColor = randomColor();
+  });
+  document.getElementById('card6').addEventListener('mouseleave',function(){
+    document.getElementById('card6').style.backgroundColor = "white";
+  })
+  
--- js/utilities.js
+++ js/utilities.js
@@ -1,12 +1,22 @@
+// This function get the value of input field
 function getInputFieldValueById (inputFieldId){
     const inputField = document.getElementById(inputFieldId);
     const inputFieldValue = inputField.value;
-    // const inputFieldValue = parseFloat(inputFieldValueString);
     inputField.value ="";
     return inputFieldValue;
-
+   
 }
+// This function set the value of area in area calculation field
 function setAreaInAreaField(areaId , areaValue){
     const areaElement = document.getElementById(areaId);
     areaElement.innerText = areaValue;
 }
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+  
